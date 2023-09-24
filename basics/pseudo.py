73dffee28016b1af5c5ae557ac3eb4bac15eb204
refilwe-m <refilwe.dev@gmail.com>
2023-09-24 20:33:38 +0200

    added a function

--- basics/pseudo.py
+++ basics/pseudo.py
@@ -1,9 +1,11 @@
 
 
-num = 11
+def even_odd(num):
 
-if num % 2 == 0:
-    print("even")
-else:
-    print("odd") 
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd") 
 
+even_odd(11)
+even_odd(2)
